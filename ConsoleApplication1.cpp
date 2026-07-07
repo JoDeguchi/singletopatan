164d40828c839c31b49c2d2e0ab54400b15aa289
JoDeguchi <jod5385@gmail.com>
2026-07-08 00:12:08 +0900

    シングルトンパターンの基礎知識について

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,20 +1,33 @@
-﻿// ConsoleApplication1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
+﻿
 #include <iostream>
+
+class GameManager
+{
+private:
+    //  コンストラクタ
+    GameManager(){}
+public:
+    //  コピーの禁止　コピーコンストラクタ
+    //  constで値を変えない
+    GameManager(const GameManager&) = delete;
+    GameManager& operator=(const GameManager&) = delete;
+
+    //  スタティック（値を保持する役割）
+    //  インスタンス１つだけで
+    static GameManager& Instance()
+    {
+        static GameManager instance;    //  C++11以降のスレッドセーフなローカル
+        return instance;
+
+    }
+    void StartGame()
+    {
+       
+        //  ゲーム開始処理
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    GameManager::Instance().StartGame();
 }
-
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
