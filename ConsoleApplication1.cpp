d200191ff47c51c942d765a27f772f1ed52d8e72
4D-16\koyo <a>
2026-07-07 14:04:43 +0900

    シングルトンパターンの作り方

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,17 +6,24 @@
 class GameManager
 {
 private:
+	//	コンストラクタ
+	//	オブジェクトを外部から生成するのを防ぐためprivateコンストラクタ
 	GameManager(){}
 public:
 
 	//	コピー代入の禁止
+	//	ゲームマネージャを値を変えないようにして参照。deleteでコピーしないように削除
+	//	コピーコンストラクタ
+	//	別のオブジェクトのコピーを禁止するため
 	GameManager(const GameManager&) = delete;
+	//	別のオブジェクトのコピーを禁止するためのコピーコンストラクタ
 	GameManager& operator=(const GameManager&) = delete;
 
+	//	スタティック領域（void と違って関数内でかいたint型変数などを保持できる）
+	//	スレッドセーフなGameManager唯一のオブジェクトをかえすことができる
 	static GameManager& Instance()
 	{
 		static GameManager instance;	//	C++11以降のスレッドセーフなローカル
-
 		return instance;
 	}
 
@@ -27,10 +34,14 @@ public:
 
 };
 
+//GameManager obj;
+
 int main()
 {
 	//	使用例
 	GameManager::Instance().StartGame();
+
+	//GameManager* instance = new GameManager();
 }
 
 
