0ef56602f14d63e93319b7f94fb5b6f324c1d32e
4D-16\koyo <a>
2026-07-07 16:05:18 +0900

    課題１つめ未完成

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,14 +2,25 @@
 //
 
 #include <iostream>
+#include <conio.h> 
 
+enum
+{
+	SCENE_OP,
+	SCENE_GAME,
+	SCENE_RESULT,
+};
 class GameManager
 {
 private:
 	//	コンストラクタ
 	//	オブジェクトを外部から生成するのを防ぐためprivateコンストラクタ
 	GameManager(){}
+	
 public:
+
+	int scene = 0;
+	char key = 0;
 
 	//	コピー代入の禁止
 	//	ゲームマネージャを値を変えないようにして参照。deleteでコピーしないように削除
@@ -27,9 +38,52 @@ public:
 		return instance;
 	}
 
+	
+
+	
+	//	ゲーム画面
 	void StartGame()
 	{
+		scene = SCENE_OP;
+
+		switch (scene)
+		{
+		case SCENE_OP:
+			
+			std::cout << "タイトル画面" << std::endl;
+			std::cout << "なにかキーをおすとゲーム開始" << std::endl;
+			
+			key = _getch();
+			if (key==32)
+			{
+				scene = SCENE_GAME;
+				key = 0;
+			}
+			
+			system("cls");
+
+				break;
+		case SCENE_GAME:
+			if (key == _getch())
+			{
+				scene = SCENE_RESULT;
+				key = 0;
+			}
+
+			std::cout << "ゲーム画面" << std::endl;
+			
+
+				break;
+		case SCENE_RESULT:
+			std::cout << "結果画面" << std::endl;
+			
+				break;
+
+
+		}
+
 		//	ゲーム開始処理
+		
 	}
 
 };
@@ -40,8 +94,6 @@ int main()
 {
 	//	使用例
 	GameManager::Instance().StartGame();
-
-	//GameManager* instance = new GameManager();
 }
 
 
